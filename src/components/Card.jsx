e523a0af74b501ea70fef57ec3c3bb8c4de99832
Matheo <matheosantana30@gmail.com>
2024-11-07 21:00:10 -0300

    css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
+import "./Card.css";
 const Card = ({ serie, personaje, episodio }) => {
   return (
-    <div>
+    <div class="card">
+      <h3>Datos obtenidos</h3>
       <p>La mejor serie: {serie}</p>
       <p>El mejor personaje: {personaje}</p>
       {episodio.trim().length != 0 && <p>El mejor episodio: {episodio}</p>}
